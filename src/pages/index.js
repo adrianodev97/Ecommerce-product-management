b41a7772b25890f1b20127a67485123cfc80628f
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-03-01 16:27:09 -0300

    update url snipcart

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,7 +64,7 @@ export default function Home({ home, products }) {
                   <Button className="snipcart-add-item"
                     data-item-id={product.id}
                     data-item-price={product.price}
-                    data-item-url={`/products/${product.slug}`}
+                    data-item-url={`/products/${product.id}`}
                     data-item-image={product.image.url}
                     data-item-name={product.name}>
                       Add to cart
